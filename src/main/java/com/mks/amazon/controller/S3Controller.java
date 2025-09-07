6aa86101d248f4fb536ec34c611586ff420ad3fa
mritunjai-mks <mritunjaims@gmail.com>
2025-09-07 14:16:35 +0530

    Project for S3 upload and download image

---
+++ src/main/java/com/mks/amazon/controller/S3Controller.java
@@ -0,0 +1,7 @@
+package com.mks.amazon.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class S3Controller {
+}
---
+++ src/test/java/com/mks/amazon/AmazonS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mks.amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
